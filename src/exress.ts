3b95284ee02c682fea0eaa0002813d5bfe753281
Klarulor <klarulor@gmail.com>
2021-10-13 19:06:23 +0300

    query added

--- src/exress.ts
+++ src/exress.ts
@@ -5,11 +5,11 @@ import { My } from "./My";
 export class Web{
     static run(){
         My.express.post(`/`, async (req: Request, res: Response) => {
-            let type = req.body?.type as reqType;
+            let type = (req.body?.type || req.query?.type) as reqType;
             if(type == "log"){
-                Discord.sendLog(req.body?.data as IMyLog);
+                Discord.sendLog((req.body?.data || req.query?.data) as IMyLog);
             }else{
-                let data = JSON.parse(req.body?.data) as IMyRequest;
+                let data = JSON.parse((req.body?.data || req.query?.data)) as IMyRequest;
                 My.servers[data.port] = data;
             }
         });
